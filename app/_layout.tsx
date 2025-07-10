f31cdb2a48f3c1261a7ddcf5e78aa93100d73590
Armykookie35 <mercyojogo@gmail.com>
2025-07-10 14:59:28 +0100

    commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
-import { Stack } from "expo-router";
+import {
+  NavigationContainer,
+  NavigationIndependentTree,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import index from "./index";
+
+const Stack = createNativeStackNavigator();
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <NavigationIndependentTree>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName="Index">
+          <Stack.Screen name="Index" component={index} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NavigationIndependentTree>
+  );
 }
