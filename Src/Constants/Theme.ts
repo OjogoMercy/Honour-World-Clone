e5bbaf9590fdbd34e0c22ce7716ab0ee79a3a2c7
Armykookie35 <mercyojogo@gmail.com>
2025-07-11 13:51:29 +0100

    Added Theme file and fonts

---
+++ Src/Constants/Theme.ts
@@ -0,0 +1,62 @@
+import { Dimensions } from "react-native";
+const { width, height } = Dimensions.get("window")
+import * as Font from "expo-font";
+
+const screenWidth = Math.round(Dimensions.get("window").width);
+const screenHeight = Math.round(Dimensions.get("window").height)
+
+export const SCREEN_WIDTH = Dimensions.get("window").width;
+export const SCREEN_HEIGHT = Dimensions.get("window").height;
+
+export const Colors = {
+  primary: "#2F75FD", //blue
+  secondary: "#0F48B8", //dark-blue
+  green: "#27ae60",
+};
+
+export const Sizes = {
+  // global sizes
+  base: screenHeight * 0.01, //for margin
+  font: screenHeight * 0.0175, //basic font size
+  radius: 5, //fixed borderRadius
+  padding: screenHeight * 0.03, //for padding
+
+  // font sizes
+  navTitle: screenHeight * 0.04375,
+  h1: screenHeight * 0.0375,
+  h2: screenHeight * 0.0275,
+  h2a: screenHeight * 0.034,
+  h2c: screenHeight * 0.0245,
+  h3: screenHeight * 0.0225,
+  h3a: screenHeight * 0.0235,
+  h4: screenHeight * 0.0175,
+  h5: screenHeight * 0.015,
+  body1: screenHeight * 0.0355,
+  body2: screenHeight * 0.025,
+  body3: screenHeight * 0.02,
+  body3a: screenHeight * 0.02,
+  body3b: screenHeight * 0.022,
+  body4: screenHeight * 0.0175,
+  body5: screenHeight * 0.015,
+  body6: screenHeight * 0.01,
+  intro: screenHeight * 0.04,
+
+  // app dimensions
+  width,
+  height,
+};
+
+
+
+export const Fonts = {
+    // custom fonts
+    regular: "GeneralSans-Regular",
+    medium: "Poppins-Medium",
+    semiBold: "Poppins-SemiBold",
+    bold: "Poppins-Bold",
+    light: "Poppins-Light",
+    extraLight: "Poppins-ExtraLight",
+}
+
+const appTheme = [Colors, Sizes, Fonts];
+export default appTheme;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,17 @@ import {
 } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import index from "./index";
+import * as Font from 'expo-font';
+import { useFonts } from 'expo-font';
 
 const Stack = createNativeStackNavigator();
+
+const [fontsLoaded] = useFonts({
+  "GeneralSans-Regular": require("./assets/fonts/GeneralSans-Regular.otf"),
+  "GeneralSans-Bold": require("../Src/assets/fonts/GeneralSans-Bold.otf"),
+  "GeneralSans-Medium": require("../Src/assets/fonts/GeneralSans-Medium.otf"),
+});
+
 
 export default function RootLayout() {
   return (
