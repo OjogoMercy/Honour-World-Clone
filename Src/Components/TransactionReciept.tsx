114d6e78f62914f1e53fbf16cf9a7a3996b4f0cb
Armykookie35 <mercyojogo@gmail.com>
2025-07-18 14:08:46 +0100

    trans

---
+++ Src/Components/TransactionReciept.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import general from "@/Src/Constants/General";
+import Icons from "@/Src/Constants/Icons";
+import { transaction } from "@/Src/Constants/Data";
+import {
+  Sizes,
+  Colors,
+  SCREEN_HEIGHT,
+  SCREEN_WIDTH,
+} from "@/Src/Constants/Theme";
+
+const TransactionReciept = () => {
+  return (
+    <View>
+      <Text>TransactionReciept</Text>
+    </View>
+  )
+}
+
+export default TransactionReciept
+
+const styles = StyleSheet.create({})
